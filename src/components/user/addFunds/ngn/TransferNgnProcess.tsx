3c957b31b77ba5c56abd502ab86903e6f39dbed5
Abukstech <abdulmajeedbamidele4@gmail.com>
2025-06-03 01:14:12 -0700

    feat fix new bugs

--- src/components/user/addFunds/ngn/TransferNgnProcess.tsx
+++ src/components/user/addFunds/ngn/TransferNgnProcess.tsx
@@ -6,7 +6,7 @@ import { CURRENCY } from "@/constants/types";
 import useUserStore from "@/store/user.store";
 import { formatNumberWithCommas } from "@/utils/utilityFunctions";
 import Image from "next/image";
-import { format } from "path";
+
 import { useState } from "react";
 import toast from "react-hot-toast";
 import { IoQrCodeSharp } from "react-icons/io5";
